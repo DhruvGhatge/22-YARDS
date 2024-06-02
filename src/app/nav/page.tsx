5407ce6dc0c53515f09130d7cd7d3fb0907fac7c
dhruvghatge <dhruvghatge11@gmail.com>
2024-06-02 11:59:38 +0530

    team page update

--- src/app/nav/page.tsx
+++ src/app/nav/page.tsx
@@ -7,25 +7,26 @@ import {
     MenubarShortcut,
     MenubarTrigger,
   } from "@/components/ui/menubar"
-  
+  import { Button } from "@/components/ui/button"
+
 
   export default function nevbar(){
     return(
+    <>
         <div className="abc">
           <div style={{ marginLeft: '20px' } }>
-  <img src="image\logo.jpg" alt="logo" style={{ maxWidth: '10%', height: '20%' }}  />
+  <img src="image\Main-Logo.png" alt="logo" style={{ maxWidth: '60px', height: '60px', borderRadius: '7px'}}  />
 </div>
-        <Menubar className="menu">
+        <Menubar className="menu" style={{borderWidth:'0px'}}>
   <MenubarMenu>
     <MenubarTrigger>Home</MenubarTrigger>
-    
   </MenubarMenu>
 
   <MenubarMenu>
     <MenubarTrigger>Live Matches</MenubarTrigger>
     <MenubarContent>
       <MenubarItem>
-       Practice Matches 
+       Practice Matches
       </MenubarItem>
       <MenubarItem>Tournament</MenubarItem>
        </MenubarContent>
@@ -60,10 +61,52 @@ import {
        Live Stream
       </MenubarItem>
       <MenubarItem>Contact Us</MenubarItem>
-      
     </MenubarContent>
+   </MenubarMenu>
+   <MenubarMenu>
+   <Button className="signin" variant="outline" style={{borderRadius:'24px', background:'#fff'}}>sign in</Button>
    </MenubarMenu>
 </Menubar>
 </div>
+<div style={{display: "flex", justifyContent: "space-between", marginLeft: "125px",
+    marginRight: "650px", marginTop: "25px", alignItems: "center"}}>
+    <div>
+        <h2 style={{fontSize: "24px"}}>Back</h2>
+    </div>
+    <div style={{display: "fleX", justifyContent: "center"}}>
+        <h1 style={{fontSize: "2.343vw"}}>Team Profile</h1>
+    </div>
+</div>
+<div className="backgroundImage">
+      <img src="image\teamProfilebanner.png"></img>
+      <div style={{marginTop: "30px", display: "flex", justifyContent: "space-between",
+    alignItems: "center", marginRight: "70px"}}>
+      <div style={{ marginLeft: '70px' } }>
+  <h1 style={{fontSize: "2.34vw"}}>Dhruv CC</h1></div>
+
+  <Menubar className="menu" style={{borderWidth:'0px', fontStyle: "2.34vw"}}>
+  <MenubarMenu>
+    <MenubarTrigger style={{fontStyle: "2.34vw"}}>Info</MenubarTrigger>
+  </MenubarMenu>
+
+  <MenubarMenu>
+    <MenubarTrigger style={{fontStyle: "2.34vw"}}>Matches</MenubarTrigger>
+  </MenubarMenu>
+
+  <MenubarMenu>
+    <MenubarTrigger style={{fontStyle: "2.34vw"}}>Profile</MenubarTrigger>
+  </MenubarMenu>
+
+  <MenubarMenu>
+    <MenubarTrigger style={{fontStyle: "2.34vw"}}>Member</MenubarTrigger>
+  </MenubarMenu>
+
+  </Menubar>
+</div>
+      </div>
+    
+
+</>
+
     )
 }
